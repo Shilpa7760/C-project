40ac87d9453a6f702c032604777aef66edb5c5c8
Shilpa7760 <shilparangaswamy7760@gmail.com>
2024-06-13 23:11:45 +0530

    Update big2.c

--- big3.c
+++ big3.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-
-void big() {
-
-  double n1, n2, n3;
-
-  printf("Enter three different numbers: ");
-  scanf("%lf %lf %lf", &n1, &n2, &n3);
-
-  // if n1 is greater than both n2 and n3, n1 is the largest
-  if (n1 >= n2 && n1 >= n3)
-    printf("%.2f is the largest number.", n1);
-
-  // if n2 is greater than both n1 and n3, n2 is the largest
-  if (n2 >= n1 && n2 >= n3)
-    printf("%.2f is the largest number.", n2);
-
-  // if n3 is greater than both n1 and n2, n3 is the largest
-  if (n3 >= n1 && n3 >= n2)
-    printf("%.2f is the largest number.", n3);
-
-  return 0;
+biggest()
+{
+    int num1, num2;
+    // Ask user to enter the two numbers
+    printf("Please Enter Two different values\n");
+    // Read two numbers from the user
+    scanf("%d %d", &num1, &num2);
+    if(num1 > num2)
+    {
+        printf("%d is Largest\n", num1);
+    }
+    else if (num2 > num1)
+    {
+        printf("%d is Largest\n", num2);
+    }
+    else
+    {
+        printf("Both are Equal\n");
+    }
+   // return 0;
 }
